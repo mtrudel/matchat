881507ada280c91e7ab3ff9a4338ef8c1787652a
Mat Trudel <mat@geeky.net>
2012-06-25 15:52:21 -0400

    random dick fix

--- plugins/dicks.rb
+++ plugins/dicks.rb
@@ -1,6 +1,6 @@
 message :body => /^\/(space)?dicks$/ do |m|
   matches = /^\/(space)?dicks$/.match(m.body)
-  length = rand(3..10)
+  length = 3 + rand(7)
   prefix = matches[1] ? ' %%>' : ''
   suffix = (rand(0...2) == 1)? ' ~o' : ''
   send_to members.except(m.from), "#{prefix}8#{'=' * length}D#{suffix}"
