75171bb78838eb75666c2666804de1c13758102c
Mat Trudel <mat@geeky.net>
2012-06-25 15:47:40 -0400

    Spacedicks

--- plugins/dicks.rb
+++ plugins/dicks.rb
@@ -1,4 +1,7 @@
-message :body => /^\/dicks$/ do |m|
+message :body => /^\/(space)?dicks$/ do |m|
+  matches = /^\/(space)?dicks$/.match(m.body)
   length = rand(3..10)
-  send_to members.except(m.from), "8#{'=' * length}D"
+  prefix = matches[1] ? ' %%>' : ''
+  suffix = (rand(0...2) == 1)? ' ~o' : ''
+  send_to members.except(m.from), "#{prefix}8#{'=' * length}D#{suffix}"
 end
