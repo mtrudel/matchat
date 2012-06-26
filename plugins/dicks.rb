6333c2c530fceb62364fe86d87cf7e9f8527a01d
Mat Trudel <mat@geeky.net>
2012-06-26 16:57:33 -0400

    compatible dicks

--- plugins/dicks.rb
+++ plugins/dicks.rb
@@ -2,6 +2,6 @@ message :body => /^\/(space)?dicks\s*$/ do |m|
   matches = /^\/(space)?dicks\s*$/.match(m.body)
   length = 3 + rand(7)
   prefix = matches[1] ? ' %%>' : ''
-  suffix = (rand(0...2) == 1)? ' ~o' : ''
+  suffix = (rand(2) == 1)? ' ~o' : ''
   send_to members.except(m.from), "#{prefix}8#{'=' * length}D#{suffix}"
 end
