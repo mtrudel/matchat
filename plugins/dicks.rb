1d7a0c5aa70ce8c1eb94f02b651b4f8b640b1c87
Mat Trudel <mat@geeky.net>
2012-07-10 21:59:51 -0400

    Better dicks

--- plugins/dicks.rb
+++ plugins/dicks.rb
@@ -1,7 +1,7 @@
-message :body => /^\/(space)?dicks\s*$/ do |m|
-  matches = /^\/(space)?dicks\s*$/.match(m.body)
-  length = 3 + rand(7)
+message :body => /^\/(space)?dicks\s*([\d])*\s*$/ do |m|
+  matches = /^\/(space)?dicks\s*([\d]*)\s*$/.match(m.body)
+  length = matches[2].length > 0 ? matches[2].to_i : (3 + rand(7))
   prefix = matches[1] ? ' %%>' : ''
   suffix = (rand(2) == 1)? ' ~o' : ''
-  send_to members.except(m.from), "#{prefix}8#{'=' * length}D#{suffix}"
+  send_to members, "[#{nick m.from}] #{prefix}8#{'=' * length}D#{suffix}"
 end
