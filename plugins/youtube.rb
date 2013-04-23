59d8d484ce96a63fd05665bfc5598791260171b7
Mat Trudel <mat@geeky.net>
2013-04-23 10:10:24 -0400

    api change

--- plugins/youtube.rb
+++ plugins/youtube.rb
@@ -4,7 +4,7 @@ require 'chronic_duration'
 
 before_broadcast_filter do |m|
   URI.extract(m.body).each do |url|
-    if (info = VideoInfo.new(url)).valid?
+    if (info = VideoInfo.get(url)).valid?
       duration = ChronicDuration.output(info.duration, :format => :chrono)
       m.body.gsub!(url, "#{url} (#{info.title} #{duration})")
       m.xhtml_body.gsub!(url, "<a href=\"#{url}\">Video: #{info.title} (#{duration})</a>")
