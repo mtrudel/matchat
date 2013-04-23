55a7dc9d1548454ae73b017f4b589a9fe9e0d815
Mat Trudel <mat@geeky.net>
2013-04-23 15:47:11 -0400

    Disble youtube

--- plugins/youtube.rb
+++ plugins/youtube.rb
@@ -2,12 +2,12 @@ require 'uri'
 require 'video_info'
 require 'chronic_duration'
 
-before_broadcast_filter do |m|
-  URI.extract(m.body).each do |url|
-    if (info = VideoInfo.get(url))
-      duration = ChronicDuration.output(info.duration, :format => :chrono)
-      m.body.gsub!(url, "#{url} (#{info.title} #{duration})")
-      m.xhtml_body.gsub!(url, "<a href=\"#{url}\">Video: #{info.title} (#{duration})</a>")
-    end
-  end
-end
+#before_broadcast_filter do |m|
+  #URI.extract(m.body).each do |url|
+    #if (info = VideoInfo.get(url))
+      #duration = ChronicDuration.output(info.duration, :format => :chrono)
+      #m.body.gsub!(url, "#{url} (#{info.title} #{duration})")
+      #m.xhtml_body.gsub!(url, "<a href=\"#{url}\">Video: #{info.title} (#{duration})</a>")
+    #end
+  #end
+#end
