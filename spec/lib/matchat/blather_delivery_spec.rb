bbcd4d85ae6883daeedaa2ea111f193a27097974
Mat Trudel <mat@geeky.net>
2013-07-24 13:51:53 -0400

    Pending tests

--- spec/lib/matchat/blather_delivery_spec.rb
+++ spec/lib/matchat/blather_delivery_spec.rb
@@ -7,4 +7,16 @@ describe Matchat::BlatherDelivery do
     subject.should.respond_to? :client
     subject.should.respond_to? :client=
   end
+
+  it 'should provide a mechanism to send out to everyone in the room' do
+    pending 'on membership'
+  end
+
+  it 'should provide a mechanism to send out to everyone in the room except the sender' do
+    pending 'on membership'
+  end
+
+  it 'should provide a mechanism to reply to sender' do
+    pending 'on membership'
+  end
 end
